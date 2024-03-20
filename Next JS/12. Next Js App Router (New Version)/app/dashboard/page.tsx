7677b56ba658f190796da6d8839358169e24d725
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-03-20 11:25:39 +0530

    Next Js App Router (New Version)

---
+++ Next JS/12. Next Js App Router (New Version)/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const DashboardPage: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold justify-center ">Dashboard Page</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ Next JS/12. Next Js App Router (New Version)/app/dashboard/profile/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const ProfilePage: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold justify-center">Profile Page</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
