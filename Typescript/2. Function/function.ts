c50bdf8768c803f6a297a803e5766fefe4c189cd
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-05-07 21:55:58 +0530

    Functions in TypeScript

--- Typescript/2. Function/function.ts
+++ Typescript/2. Function/function.ts
@@ -1,19 +1,19 @@
-// function addTwo(num: number) {
-//   return num + 2;
-// }
+function addTwo(num: number) {
+  return num + 2;
+}
 
-// function getUpper(val: string) {
-//   return val.toUpperCase();
-// }
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
 
-// function signUpUser(name: string, email: string, isPaid: boolean) {}
+function signUpUser(name: string, email: string, isPaid: boolean) {}
 
-// let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
-// addTwo(5);
-// getUpper("4");
-// signUpUser("avi", "avi@gmail.com", false);
-// loginUser("AVI", "avi@gmail.com")
+addTwo(5);
+getUpper("4");
+signUpUser("avi", "avi@gmail.com", false);
+loginUser("AVI", "avi@gmail.com")
 
 
-// export {};
+export {};
