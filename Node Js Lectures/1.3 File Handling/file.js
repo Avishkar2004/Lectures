6b90bb280a9b867f579cd74064ed2331335d2ae8
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-05-25 10:42:59 +0530

    FS Module

---
+++ Node Js Lectures/1.3 File Handling/file.js
@@ -0,0 +1,55 @@
+const fs  = require("fs")
+
+
+//Sync...
+// fs.writeFileSync('./text.txt', "hey There")
+
+//Async...
+// fs.writeFile('./text.txt', "hey ", (err) => {})
+
+
+//Create contact.txt file manually first before running this code
+//How to read File ?
+// const result = fs.readFileSync("./contact.txt", "utf-8")
+// console.log(result)
+
+
+// fs.readFile("./contact.txt", "utf-8", (err, res) => {
+//     if(err) {
+//         console.log("Error", err )
+//     } else {
+//         console.log(res)
+//     }
+// })
+
+
+// AppendFile :-
+
+// fs.appendFileSync("./text.txt", new Date().getDate().toLocaleString());
+
+fs.appendFileSync("./text.txt", `${Date.now()} Hey There\n`); 
+// 251716613467411 Hey There
+// 1716613468136 Hey There
+// 1716613479849 Hey There
+
+
+// Copy File
+// fs.cpSync("./text.txt", "./copy.txt") //This will generate copy file
+
+
+
+//Delete File
+// fs.unlinkSync("./copy.txt") //this command will delete copy.txt file
+
+
+//Shows the status of file
+// console.log(fs.statSync("./text.txt")) 
+
+
+//make directory(folder)
+// fs.mkdirSync("mydocs")
+
+// Folder in folder
+fs.mkdirSync("mydocs/avish", {recursive : true}) 
+
+//! U can't do this in normal js bcoz of security purposes
---
+++ Node Js Lectures/1.3 File Handling/math.js
@@ -0,0 +1,20 @@
+
+//! 1 approach
+// function add(a, b) {
+//     return a + b;
+//   }
+  
+//   function sub(a, b) {
+//     return a - b;
+//   }
+  
+  
+//   //! Multiple Export / default
+//   module.exports = { add, sub };
+  
+
+//! 2 Approach
+
+exports.add1 = (a , b) => a + b
+
+exports.sub2 = (a ,b) => a - b
