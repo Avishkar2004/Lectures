5dec8cf3b8ddda5006ec3a272a22f5843b42f419
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-05-25 18:44:47 +0530

    Architecture of Node Js

---
+++ Node Js Lectures/4. Architecture Of Node Js/file.js
@@ -0,0 +1,27 @@
+const fs  = require("fs")
+const os = require("os")
+
+console.log(os.cpus().length) //12 
+//! blocking req
+// console.log("first")
+// const result = fs.readFileSync("./contact.txt", "utf-8")
+// console.log(result)
+// console.log("second")
+
+// => first
+// Avi :- 2540216352165
+// second
+
+
+
+console.log("first")
+//! non- blocking req
+fs.readFile("./contact.txt", "utf-8", (err, result) =>{
+    console.log(result)
+})
+console.log("second")
+
+
+// => first
+// second
+// Avi :- 2540216352165
