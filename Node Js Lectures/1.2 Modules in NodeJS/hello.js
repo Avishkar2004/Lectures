8c2e0c473e089ff1c6625810ae8b97e14bddd66e
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-05-25 10:06:24 +0530

    Modules in NodeJS

---
+++ Node Js Lectures/1.2 Modules in NodeJS/hello.js
@@ -0,0 +1,16 @@
+const math = require('./math');
+
+// console.log(math.add(2,3))
+// console.log(math.sub(3,0))
+
+console.log(math.add1(2,3))
+console.log(math.sub2(3,0))
+
+
+
+//! 2 approach
+// const {add, sub} = require('./math');
+
+
+// console.log(add(2,3))
+// console.log(sub(3,0))
